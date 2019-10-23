b9f7b75ac7945714118b14ceb3ea8f41b77fe54d
kevinmgarvey <53451615+kevinmgarvey@users.noreply.github.com>
2019-10-23 13:37:00 -0700

    Add files via upload

---
+++ capstone.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> Notebook for Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
